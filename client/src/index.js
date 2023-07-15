ade0821d7944855e4e155d1d64add737750b68bc
nishant4392 <nishantchandel789@gmail.com>
2023-07-15 11:27:22 +0530

    google auth trial

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <GoogleOAuthProvider clientId="203585739757-l4roiulufkluko05puu0o1a3adrl7u41.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </BrowserRouter>
-  </React.StrictMode>
 );
