035daa6bdedcf11fa7e4a32edb29fbda1a304dbb
nishant4392 <nishantchandel789@gmail.com>
2023-07-29 10:23:52 +0530

    finalized on tailwind css and flowbite component library

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className=' border-blue-800 border-4'>
-      Login page
+    <div className="">
+      <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-black to-purple-500 group-hover:from-black group-hover:to-purple-500 hover:text-white dark:text-white ">
+        <span
+          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 	
+          text-3xl"
+        >
+          Purple to pink
+        </span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
